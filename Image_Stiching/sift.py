52a838d938cf8b14fb4eff3496602e8226013281
jtresman <jacob.resman@colorado.edu>
2016-01-26 16:51:44 -0700

    It Works!

--- Image_Stiching/sift.py
+++ Image_Stiching/sift.py
@@ -7,6 +7,16 @@ gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 sift = cv2.xfeatures2d.SIFT_create()
 kp = sift.detect(gray,None)
    
-img=cv2.drawKeypoints(gray,kp)
+# This is the zeroed out mnumpy image
+drawing = np.zeros(img.shape,np.uint8)
+
+# This creates the uniq points 
+#  with circles on the original image
+output=cv2.drawKeypoints(gray,kp,drawing)
    
-cv2.imwrite('sift_keypoints.jpg',img)
+cv2.imwrite('sift_keypoints.jpg',output)
+
+# Show the output image
+cv2.imshow("Output", output)
+
+cv2.waitKey(0)
