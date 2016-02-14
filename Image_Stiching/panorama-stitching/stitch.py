da2782430dd6c4058175ff2e09c7190d4f10492f
Zak Collins <foazak@yahoo.com>
2016-02-14 16:44:11 -0700

    Update stitch.py
    Puts stitching code into a function so that it can be called when stitching 4 images

--- Image_Stiching/panorama-stitching/stitch.py
+++ Image_Stiching/panorama-stitching/stitch.py
@@ -7,6 +7,12 @@ import argparse
 import imutils
 import cv2
 
+'''
+NOTES:
+	
+	
+'''
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--first", required=True,
@@ -14,24 +20,36 @@ ap.add_argument("-f", "--first", required=True,
 ap.add_argument("-s", "--second", required=True,
 	help="path to the second image")
 args = vars(ap.parse_args())
+stitch_images(args["first"], args["second"])
 
-# load the two images and resize them to have a width of 400 pixels
-# (for faster processing)
-imageA = cv2.imread(args["first"])
-imageB = cv2.imread(args["second"])
-imageA = imutils.resize(imageA, width=400)
-imageB = imutils.resize(imageB, width=400)
-
-# stitch the images together to create a panorama
-stitcher = Stitcher()
-(result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
-
-# save the image
-cv2.imwrite('result.jpg', result)
-
-# show the images
-cv2.imshow("Image A", imageA)
-cv2.imshow("Image B", imageB)
-cv2.imshow("Keypoint Matches", vis)
-cv2.imshow("Result", result)
-cv2.waitKey(0)
+def stitch_images(name1, name2):
+    ''' Stitches 2 images together
+        
+    Args:
+        name1: Name of first image
+        name2: Name of second image
+    
+    Returns: 
+        None
+    
+    '''
+	# load the two images and resize them to have a width of 400 pixels
+	# (for faster processing)
+	imageA = cv2.imread(name1)
+	imageB = cv2.imread(name2)
+	imageA = imutils.resize(imageA, width=400)
+	imageB = imutils.resize(imageB, width=400)
+	
+	# stitch the images together to create a panorama
+	stitcher = Stitcher()
+	(result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
+	
+	# save the image
+	cv2.imwrite('result.jpg', result)
+	
+	# show the images
+	cv2.imshow("Image A", imageA)
+	cv2.imshow("Image B", imageB)
+	cv2.imshow("Keypoint Matches", vis)
+	cv2.imshow("Result", result)
+	cv2.waitKey(0)
