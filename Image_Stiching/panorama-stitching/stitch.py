9cc12f7b12e257fae03c112a9ea5a8e1131b1130
Zak Collins <foazak@yahoo.com>
2016-02-14 16:21:18 -0700

    Update stitch.py
    imshow results in an error when using ssh to use the pi, so moving that to after the image saving.

--- Image_Stiching/panorama-stitching/stitch.py
+++ Image_Stiching/panorama-stitching/stitch.py
@@ -26,12 +26,12 @@ imageB = imutils.resize(imageB, width=400)
 stitcher = Stitcher()
 (result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
 
+# save the image
+cv2.imwrite('result.jpg', result)
+
 # show the images
 cv2.imshow("Image A", imageA)
 cv2.imshow("Image B", imageB)
 cv2.imshow("Keypoint Matches", vis)
-
-# save the image
-cv2.imwrite('result.jpg', result)
 cv2.imshow("Result", result)
 cv2.waitKey(0)
