3d89942a3820c4a4694ceaad12fd45b0a2e4d476
jtresman <jacob.resman@colorado.edu>
2016-02-22 10:32:01 -0700

    Clean Up

--- Image_Stiching/panorama-stitching/stitch.py
+++ Image_Stiching/panorama-stitching/stitch.py
@@ -1,5 +1,5 @@
 # USAGE
-# python stitch.py --first images/bryce_left_01.png --second images/bryce_right_01.png 
+# python stitch.py --first images/bryce_left_01.png --second images/bryce_right_01.png
 
 # import the necessary packages
 from pyimagesearch.panorama import Stitcher
@@ -9,10 +9,8 @@ import cv2
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--first", required=True,
-	help="path to the first image")
-ap.add_argument("-s", "--second", required=True,
-	help="path to the second image")
+ap.add_argument("-f", "--first", required=True, help="path to the first image")
+ap.add_argument("-s", "--second", required=True, help="path to the second image")
 args = vars(ap.parse_args())
 
 # load the two images and resize them to have a width of 400 pixels
